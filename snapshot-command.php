8670105a5782ecf8e17a14d464f9faf4e68236fd
Abhijit Rakas <abhijit.rakas@rtcamp.com>
2020-08-29 19:19:00 +0530

    Show errors for snapshot command required missing packages

--- snapshot-command.php
+++ snapshot-command.php
@@ -1,9 +1,24 @@
 <?php
+
 if ( ! class_exists( 'WP_CLI' ) ) {
 	return;
 }
+
 $wpcli_snapshot_autoloader = __DIR__ . '/vendor/autoload.php';
+
 if ( file_exists( $wpcli_snapshot_autoloader ) ) {
 	require_once $wpcli_snapshot_autoloader;
 }
-WP_CLI::add_command( 'snapshot', '\WP_CLI\Snapshot\SnapshotCommand' );
+
+$wpcli_snapshot_verify_required_packages = function() {
+	$utils = new \WP_CLI\Snapshot\Utils();
+	$utils->available_wp_packages();
+};
+
+WP_CLI::add_command(
+	'snapshot',
+	'\WP_CLI\Snapshot\SnapshotCommand',
+	[
+		'before_invoke' => $wpcli_snapshot_verify_required_packages,
+	]
+);
